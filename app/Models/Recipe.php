eac70437a621b12731c1c3361ea1d1d5f80193ff
Tim Burns <tim.marshall.burns95@gmail.com>
2021-11-26 17:42:12 -0500

    worked on recipe model

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Recipe extends Model
 {
     use HasFactory;
+
+//  protected $table = 'students';
+//  protected $primaryKey = 'id';
+//  public $timestamps = false;
+//TODO - add id?, name, description, ingredients, steps, website, comments, calories, servings, rating,
+//TODO -            country_id, user_id, restaurant_id, created_at, updated_at
+    protected $fillable = ['name', 'description', 'ingredients', 'steps', 'website', 'comments',
+        'calories', 'servings', 'rating', 'country_id', 'user_id', 'restaurant_id'];
 }
