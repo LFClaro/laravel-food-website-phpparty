a28d8caeb2cf0ac7ac4d1521afba22b00277517b
LFClaro <lfclaro@gmail.com>
2021-12-01 15:02:55 -0500

    adjusting logo aspect ratio in nav bar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -26,7 +26,7 @@
     <header class="header">
         <nav class="navbar navbar-expand-lg fixed-top py-3">
             <div class="container">
-                    <a href="/home"><img src="{{asset('/img/Logo_transparent.png')}}" alt="PHP Party" width="100" height="50"></a>
+                    <a href="/home"><img src="{{asset('/img/Logo_transparent.png')}}" alt="PHP Party" height="50"></a>
                     <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right">
                         <i class="fa fa-bars"></i></button>
                     @include('inc.nav')
