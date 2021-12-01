8e445af9a9f0d4f2a7c26e7ab15031744d4cd2e4
LFClaro <lfclaro@gmail.com>
2021-12-01 14:23:38 -0500

    recipe layout change

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -10,29 +10,30 @@
 @section('content')
     <div class="container align-items-center justify-content-center">
         <h1 class="text-center">Recipe List</h1>
+        <p class="text-center"><a href="{{ url("recipes/create") }}" class="btn btn-success text-light">Add Recipe</a></p>
         <table class="table table-striped table-light text-dark table-hover">
-            <thead>
+            <thead class="text-center text-black text-capitalize">
             <tr>
-                <th>NAME</th>
+                <th>Name</th>
                 <th>Description</th>
                 <th>Website</th>
                 <th>Calories</th>
                 <th>Servings</th>
                 <th>Rating</th>
                 <th>Country</th>
-                <th colspan="2">OPTIONS</th>
+                <th colspan="2">Options</th>
             </tr>
             </thead>
             <tbody>
             @foreach($recipes as $r)
                 <tr onclick="window.location='{{ url('recipes', $r->id) }}';">
-                    <td><a href="{{ url('recipes', $r->id) }}" class="text-light">{{ $r->name }}</a></td>
-                    <td><a href="{{ url('recipes', $r->id) }}" class="text-light">{{ $r->description }}</a></td>
-                    <td><a href="{{ $r->website }}" class="text-light">{{ $r->website }}</a></td>
-                    <td><a href="{{ url('recipes', $r->id) }}" class="text-light">{{ $r->calories }}</a></td>
-                    <td><a href="{{ url('recipes', $r->id) }}" class="text-light">{{ $r->servings }}</a></td>
-                    <td><a href="{{ url('recipes', $r->id) }}" class="text-light">{{ $r->rating }}</a></td>
-                    <td><a href="{{ url('recipes', $r->id) }}" class="text-light">{{ $r->country->name}}</a></td>
+                    <td><a href="{{ url('recipes', $r->id) }}" class="link-info text-decoration-none">{{ $r->name }}</a></td>
+                    <td>{{ $r->description }}</td>
+                    <td><a href="{{ $r->website }}" class="link-info text-decoration-none">{{ $r->website }}</a></td>
+                    <td>{{ $r->calories }}</td>
+                    <td>{{ $r->servings }}</td>
+                    <td>{{ $r->rating }}</td>
+                    <td>{{ $r->country->name}}</td>
                     <td>
                         <form method="get" action="{{ url('recipes/' . $r->id . '/edit')}}">
                             @csrf
@@ -51,7 +52,6 @@
             </tbody>
         </table>
         <br />
-        <p><a href="{{ url("recipes/create") }}" class="btn btn-success me-2 float-right">Add Recipe</a></p>
     </div>
 @endsection
 @section('footer')
