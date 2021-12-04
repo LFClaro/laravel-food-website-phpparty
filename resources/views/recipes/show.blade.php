6ea20a61967924068cf48b33d13ea9fc303d6831
Tim Burns <tim.marshall.burns95@gmail.com>
2021-12-04 12:13:48 -0500

    user id needed

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -24,32 +24,31 @@
         @endforeach
         <br /><br />
 
-    </div>
-    <div class="container align-items-center justify-content-center">
-        <h1>Add Review </h1>
-        <form action="{{ url('recipereview') }}" method="post">
-            @csrf
-            <div class="form-group">
-                <label for="userid">User Id: </label>
-                <input type="text" class="form-control" name="userid" id="userid"
-                       placeholder="Enter Name" />
-            </div>
+        <!--TODO: need to link with user id to add comments -->
+{{--    </div>--}}
+{{--    <div class="container align-items-center justify-content-center">--}}
+{{--        <h1>Add Review </h1>--}}
+{{--        <form action="{{ url('recipereview') }}" method="post">--}}
+{{--            @csrf--}}
+{{--            <div class="form-group">                --}}
+{{--                <input type="hidden" class="form-control" name="userid" id="userid"--}}
+{{--                       value="{{}}" />--}}
+{{--            </div>--}}
 {{--            <div class="form-group">--}}
-{{--                <label for="recipeid">Recipe Id: </label>--}}
-{{--                <input type="text" class="form-control" name="recipeid" id="recipeid"--}}
-{{--                       placeholder="Enter description" />--}}
-            </div>
-            <div class="form-group">
-                <label for="comment">Ingredients: </label>
-                <textarea type="text" class="form-control" name="comment" id="comment"
-                          placeholder="Enter ingredients"></textarea>
-            </div>
-            <br>
-            <input type="submit" name="addRecipe"
-                   class="btn btn-success float-right" id="btn-submit" value="Comment" />
+{{--                <input type="hidden" class="form-control" name="recipeid" id="recipeid"--}}
+{{--                       placeholder="Enter description"  value="{{$review->recipe_id}}"/>--}}
+{{--            </div>--}}
+{{--            <div class="form-group">--}}
+{{--                <label for="comment">Comment: </label>--}}
+{{--                <textarea type="text" class="form-control" name="comment" id="comment"--}}
+{{--                          placeholder="Enter comments"></textarea>--}}
+{{--            </div>--}}
+{{--            <br>--}}
+{{--            <input type="submit" name="addRecipeReview"--}}
+{{--                   class="btn btn-success float-right" id="btn-submit" value="Comment" />--}}
 
 
-            <a href="{{ url('recipes') }}" id="btn_back" class="btn btn-success float-right">Back</a>
-        </form>
-    </div>
+{{--            <a href="{{ url('recipes') }}" id="btn_back" class="btn btn-success float-right">Back</a>--}}
+{{--        </form>--}}
+{{--    </div>--}}
 @endsection
