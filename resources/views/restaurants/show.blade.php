d9b11145373f226766052ef03511eaf630934104
Tim Burns <tim.marshall.burns95@gmail.com>
2021-12-07 13:59:04 -0500

    finished getting weather for city

--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -1,3 +1,34 @@
+<?php
+$apiKey = "4b0d1232cca66e6ab418389b2cbd1a5c";
+//TODO need to split address on comas and read second split - address, city, postal code
+//TODO - issues with richmond hill or cities with space in them - %20
+$string =$restaurants->address; // set string we want to use
+$str_arr = preg_split("/\,/", $string); // split the sting on commas
+$spaces = trim($str_arr[1]); // trim off the white space
+$cityId = str_replace(" ", "%20", $spaces); // gets rid of spaces between words for url
+$OWMApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" . $cityId . "&units=metric&APPID=" . $apiKey;
+//$OWMApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=Richmond%20Hill&units=metric&APPID=" . $apiKey;
+// pass in our strings need to complete url
+
+$ch = curl_init();
+// Returns a cURL handle on success, false on errors.
+
+// curl_setopt sets the URL and other appropriate options
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $OWMApiUrl);// pass in our open weather api api string
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
+// grabs URL and pass it to the browser
+
+curl_close($ch); // close the resource and frees up some space
+$data = json_decode($response); // set json object for work
+$currentTime = time(); // get current time, maybe will use but will have to format
+// now can work with data
+?>
+
 @extends('layouts.main')
 
 @section( 'title')
@@ -15,6 +46,30 @@
                 <p>{{$review->comment}}</p>
             @endif
         @endforeach
+        <br>
+        <h3 class="text-warning">Current Weather in {{$str_arr[1]}}</h3>
+        <p><?php echo date("l, jS F, Y",$currentTime); ?></p>
+        <p>
+            <?php echo ucwords($data->weather[0]->description); ?>
+                <img
+                src="http://openweathermap.org/img/w/<?php echo $data->weather[0]->icon; ?>.png"
+                class="weather-icon"
+                />
+        </p>
+        <p class="text-warning">
+             Max Temp: <?php echo $data->main->temp_max; ?>&deg;C
+            <span
+                class="text-danger">Min Temp: <?php echo $data->main->temp_min; ?>&deg;C
+            </span>
+        </p>
+        <p>Humidity: <?php echo $data->main->humidity; ?> %</p>
+        <p>Wind: <?php echo $data->wind->speed; ?> km/h</p>
+        <br><br>
         <a href="{{ url('restaurants') }}" id="btn_back" class="btn btn-success float-right">Back</a>
     </div>
+    <!--TODO testing below -->
+{{--    <p>{{$str_arr[1]}}</p>--}}
+{{--    <p><?php var_dump($data); ?></p>--}}
+{{--    <p>{{$cityId}}</p>--}}
+
 @endsection
